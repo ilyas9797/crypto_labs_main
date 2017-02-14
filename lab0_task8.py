08f05bb0ddd792a46765a3dbf20853a278416394
Ilyas Khairullin <ildar97.97@gmail.com>
2017-02-15 02:25:28 +0300

    Finished task 8

--- lab0_task8.py
+++ lab0_task8.py
@@ -7,18 +7,39 @@ from binascii import a2b_hex
 
 bin_lines_list = [a2b_hex(x) for x in file_lines_list]
 
+lines_list = []
+
+for i in range(len(bin_lines_list) - 1):
+    lines_list.append('')
+    for j in range(len(bin_lines_list[i])):
+        lines_list[i] += chr(bin_lines_list[i][j])
 
 
-import re
 
-list_of_num_encrypt_lines = []
+for k in lines_list:
+    list_i_matchings = []
+    for n in range(len(k) - 16):
+        s_n = k[ n : n + 16 : ]
+        found_start_index = str(k).find(s_n, n + 16)
+        if found_start_index != -1:
+            if len(list_i_matchings) == 0:
+                list_i_matchings.append(n)
+            list_i_matchings.append(found_start_index)
 
-for s in range(len(lines_list) - 1):
-    l = len(s)
-    num_iters = int(l // 16)
-    for i in range(num_iters):
-        pattern_line = lines_list[i : i + 16 :]
-        p = re.compile(pattern_line)
-        m = p.findall(pattern_line)
+    if len(list_i_matchings) != 0:
+        print('Line: ', str(lines_list.index(k)))
+        print(' Matching substring: ')
+        print('---------------------')
+        print('     Byte line:')
+        print('---------------------')
+        print(str(k[list_i_matchings[0]: list_i_matchings[0] + 16: ]).encode())
+        print('---------------------')
+        print('     String:')
+        print('---------------------')
+        print(str(k[list_i_matchings[0]: list_i_matchings[0] + 16: ]))
+        print('---------------------')
+        for m in list_i_matchings:
+            print('(' + str(m) + ', ' + str(m + 16) + ')')
+
 
 
--- test.py
+++ test.py
@@ -57,7 +57,7 @@ class Fibonacci:
 for i in Fibonacci(100):
   print(i)
 '''
-
+'''
 l = [1234567890, 7654421496, 42325267213, 9823412461, 11222277422346, 73453282345, 9937651836, 42, 666222115293, 8211111143445]
 
 n = int(0)
@@ -68,3 +68,47 @@ for i in range(len(l)):
             if((l[i] < l[j] or l[i] > l[j]) and ((l[i] + l[j]) % 3 == 0)):
                 n += 1
 print(n)
+'''
+'''
+stack = []
+
+s = input()
+
+error_marker = False
+
+for i in s:
+    if i == '(' or i == ')':
+        if i == '(':
+            stack.append(i)
+        else:
+            if len(stack) == 0 or '(' != stack.pop():
+                error_marker = True
+                break
+    elif i == '[' or i == ']':
+        if i == '[':
+            stack.append(i)
+        else:
+            if len(stack) == 0 or '[' != stack.pop():
+                error_marker = True
+                break
+    elif i == '{' or i == '}':
+        if i == '{':
+            stack.append(i)
+        else:
+            if len(stack) == 0 or '{' != stack.pop():
+                error_marker = True
+                break
+    else:
+        if i == '<':
+            stack.append(i)
+        else:
+            if len(stack) == 0 or '<' != stack.pop():
+                error_marker = True
+                break
+if error_marker == True or len(stack) != 0:
+    print('NO')
+else:
+    print('YES')
+'''
+
+
